876fe9ab075482f8038e2b0905df41f5c6abf870
Daniela Martinez <dmartmax@hotmail.com>
2022-10-17 16:20:31 -0500

    Add my first function for adding numbers

--- Main-Project/assets/scripts/app.js
+++ Main-Project/assets/scripts/app.js
@@ -1,13 +1,20 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
-
-//let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1' 
-// -- same as the comand below but in a extended way
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
+//currentResult = ((currentResult + 10) * 3) / 2 - 1; -- using simple operators
+//let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1' -- using single quote.
+//let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`; -- using `
 //let errorMessage = 'An error \n' + 'ocurred!' -- \n for line break
+//outputResult(currentResult,errorMessage) -- for displaying the error message instead the calculation description
 
-outputResult(currentResult,calculationDescription);
-//outputResult(currentResult,errorMessage)
-// -- for displaying the error message instead the calculation description
+
+// --- Functions ---
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+currentResult = add(5,15);
+
+const outputDescription = 'That was an easy one!'
+outputResult(currentResult, outputDescription);
+
