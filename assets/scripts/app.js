738bf624a837adb01921d4a7c3369956519248c7
Daniela Martinez <dmartmax@hotmail.com>
2022-10-17 13:26:24 -0500

    Add different data types

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,13 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-currentResult = currentResult + 10 * 3;
+currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-outputResult(currentResult,'');
+//let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1' 
+// -- same as the comand below but in a extended way
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
+//let errorMessage = 'An error \n' + 'ocurred!' -- \n for line break
+
+outputResult(currentResult,calculationDescription);
+//outputResult(currentResult,errorMessage)
+// -- for displaying the error message instead the calculation description
