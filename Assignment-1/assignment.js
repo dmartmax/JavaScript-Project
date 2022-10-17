773dbf21a85bde566bd19ed1cdc742dbec5c87dc
Daniela Martinez <dmartmax@hotmail.com>
2022-10-17 13:58:24 -0500

    Complete Assignment 1

---
+++ Assignment-1/assignment.js
@@ -0,0 +1,14 @@
+const userInput =  25;
+let result;
+
+result = userInput + 18;
+
+result = result - 9;
+result = result * 10;
+result = result / 2;
+
+userInputMessage = `The user input is ${userInput}`;
+resultMessage = `The result is ${result}`;
+
+alert(userInputMessage);
+alert(resultMessage);
