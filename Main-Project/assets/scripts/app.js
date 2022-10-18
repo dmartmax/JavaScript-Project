7fbb7fa19a81d75e8e0127321528fc66bce5bf6c
Daniela Martinez <dmartmax@hotmail.com>
2022-10-18 10:52:28 -0500

    Add objects for operation

--- Main-Project/assets/scripts/app.js
+++ Main-Project/assets/scripts/app.js
@@ -1,11 +1,14 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let allEntries = [];
 
-//currentResult = ((currentResult + 10) * 3) / 2 - 1; -- using simple operators
-//let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1' -- using single quote.
-//let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`; -- using `
-//let errorMessage = 'An error \n' + 'ocurred!' -- \n for line break
-//outputResult(currentResult,errorMessage) -- for displaying the error message instead the calculation description
+/*
+currentResult = ((currentResult + 10) * 3) / 2 - 1; -- using simple operators
+let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1' -- using single quote.
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`; -- using `
+let errorMessage = 'An error \n' + 'ocurred!' -- \n for line break
+outputResult(currentResult,errorMessage) -- for displaying the error message instead the calculation description
+*/
 
 // --- Functions ---
 
@@ -16,29 +19,45 @@ function getUserInput() {
 function add() {
   const inputNumber = getUserInput();
   const initialResult = currentResult;
-  currentResult = currentResult + inputNumber;
+  currentResult += inputNumber;
   createOutput("+", initialResult, inputNumber);
+  createObject("Add", initialResult, inputNumber, currentResult);
 }
 
 function subtract() {
   const inputNumber = getUserInput();
   const initialResult = currentResult;
-  currentResult = currentResult - inputNumber;
+  currentResult -= inputNumber;
   createOutput("-", initialResult, inputNumber);
+  createObject("Subtract", initialResult, inputNumber, currentResult);
 }
 
 function multiply() {
   const inputNumber = getUserInput();
   const initialResult = currentResult;
-  currentResult = currentResult * inputNumber;
+  currentResult *= inputNumber;
   createOutput("x", initialResult, inputNumber);
+  createObject("Multiply", initialResult, inputNumber, currentResult);
 }
 
 function divide() {
   const inputNumber = getUserInput();
   const initialResult = currentResult;
-  currentResult = currentResult / inputNumber;
+  currentResult /= inputNumber;
   createOutput("/", initialResult, inputNumber);
+  createObject("Divide", initialResult, inputNumber, currentResult);
+}
+
+function createObject(operator, prevResult, inputValue, newResult) {
+  const newEntry = {
+    operation: operator,
+    oldResult: prevResult,
+    entryNumber: inputValue,
+    result: newResult,
+  };
+
+  allEntries.push(newEntry);
+  console.log(allEntries);
 }
 
 function createOutput(operator, prevResult, inputValue) {
