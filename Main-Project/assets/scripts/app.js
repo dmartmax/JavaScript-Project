4240082b920ab3269a6ecfd46733b3928bbf8241
Daniela Martinez <dmartmax@hotmail.com>
2022-10-18 08:58:51 -0500

    Add all the operations

--- Main-Project/assets/scripts/app.js
+++ Main-Project/assets/scripts/app.js
@@ -1,6 +1,5 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-const outputDescription = 'That was an easy one!'
 
 //currentResult = ((currentResult + 10) * 3) / 2 - 1; -- using simple operators
 //let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1' -- using single quote.
@@ -8,11 +7,46 @@ const outputDescription = 'That was an easy one!'
 //let errorMessage = 'An error \n' + 'ocurred!' -- \n for line break
 //outputResult(currentResult,errorMessage) -- for displaying the error message instead the calculation description
 
-
 // --- Functions ---
+
+function getUserInput() {
+  return parseInt(userInput.value);
+}
+
 function add() {
-  currentResult = currentResult + userInput.value;
+  const inputNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + inputNumber;
+  createOutput("+", initialResult, inputNumber);
+}
+
+function subtract() {
+  const inputNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - inputNumber;
+  createOutput("-", initialResult, inputNumber);
+}
+
+function multiply() {
+  const inputNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * inputNumber;
+  createOutput("x", initialResult, inputNumber);
+}
+
+function divide() {
+  const inputNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / inputNumber;
+  createOutput("/", initialResult, inputNumber);
+}
+
+function createOutput(operator, prevResult, inputValue) {
+  const outputDescription = `${prevResult} ${operator} ${inputValue}`;
   outputResult(currentResult, outputDescription);
 }
 
-addBtn.addEventListener('click', add);
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
